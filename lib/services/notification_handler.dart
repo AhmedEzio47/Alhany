0d1981f8c5df55ee647e41cbc804b4bddbbb3dbe
Ahmed <ahmednab93@gmail.com>
2021-01-31 13:30:27 +0200

    - Firebase upgrade

--- lib/services/notification_handler.dart
+++ lib/services/notification_handler.dart
@@ -14,10 +14,42 @@ import 'database_service.dart';
 
 class NotificationHandler {
   static receiveNotification(
-      BuildContext context, GlobalKey<ScaffoldState> scaffoldKey) {
+      BuildContext context, GlobalKey<ScaffoldState> scaffoldKey) async {
     StreamSubscription iosSubscription;
-    FirebaseMessaging _fcm = FirebaseMessaging();
+    await FirebaseMessaging.instance
+        .setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
 
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage message) {
+      if (message != null) {
+        navigateToScreen(
+            context, message.data['type'], message.data['object_id']);
+      }
+    });
+
+    /// Initalize the [FlutterLocalNotificationsPlugin] package.
+    final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      navigateToScreen(
+          context, message.data['type'], message.data['object_id']);
+    });
     // if (Platform.isIOS) {
     //   iosSubscription = _fcm.onIosSettingsRegistered.listen((data) {
     //     // save the token  OR subscribe to a topic here
