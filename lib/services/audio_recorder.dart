3f0bd83ecf634b0da955ee2a2ca72920cb21983b
Ahmed <ahmednab93@gmail.com>
2021-04-03 23:36:20 +0200

    -Removed sounds package

--- lib/services/audio_recorder.dart
+++ lib/services/audio_recorder.dart
@@ -1,49 +1,57 @@
 import 'dart:async';
 
-import 'package:sounds/sounds.dart';
-
-enum RecordingStatus { Recording, Stopped, Unset }
+import 'package:Alhany/constants/strings.dart';
+import 'package:flutter_audio_recorder/flutter_audio_recorder.dart';
 
 class AudioRecorder {
-  SoundRecorder _recorder;
-  String _recording;
+  FlutterAudioRecorder _recorder;
+  Recording _recording;
   Timer timer;
-  Track _track;
 
   AudioRecorder() {
     init();
   }
 
   void init() async {
-    // String customPath = appTempDirectoryPath;
-    // customPath = customPath + DateTime.now().millisecondsSinceEpoch.toString();
+    String customPath = appTempDirectoryPath;
 
-    _recording = Track.tempFile(WellKnownMediaFormats.adtsAac);
+    // can add extension like ".mp4" ".wav" ".m4a" ".aac"
+    customPath = customPath + DateTime.now().millisecondsSinceEpoch.toString();
 
-    _recorder = SoundRecorder();
+    // .wav <---> AudioFormat.WAV
+    // .mp4 .m4a .aac <---> AudioFormat.AAC
+    // AudioFormat is optional, if given value, will overwrite path extension when there is conflicts.
 
-    _track =
-        Track.fromFile(_recording, mediaFormat: WellKnownMediaFormats.adtsAac);
-
-    _recorder.onStopped = ({wasUser}) async {
-      await _recorder.release();
-
-      /// recording has finished so play it back to the user.
-      print(_recording);
-      //File(_recording).delete();
-    };
+    _recorder = FlutterAudioRecorder(
+      customPath,
+      audioFormat: AudioFormat.WAV,
+      sampleRate: 44100,
+    );
+    await _recorder.initialized;
   }
 
-  Future startRecording() async {
-    await _recorder.record(_track);
+  Future startRecording({var conversation}) async {
+    await _recorder.start();
+    var current = await _recorder.current();
+
+    _recording = current;
+
+    Timer.periodic(Duration(milliseconds: 10), (Timer t) async {
+      var current = await _recorder.current();
+
+      _recording = current;
+      timer = t;
+    });
   }
 
   Future<String> stopRecording() async {
-    await _recorder.stop();
-    return _recording;
+    if (timer != null) {
+      timer.cancel();
+    }
+    Recording result = await _recorder.stop();
+    _recording = result;
+    return result.path;
   }
 
-  dispose() async {
-    //await _recorder.release();
-  }
+  Future dispose() async {}
 }
