9d38449a7ffc2fe4b769489d41d2fdaab8c341a9
EidarousDev <eidarousdev@gmail.com>
2021-10-31 05:21:57 -0700

    RevenueCat takes user_id as in input

--- lib/provider/revenuecat.dart
+++ lib/provider/revenuecat.dart
@@ -1,34 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 
-enum Entitlement {free, exclusives}
-class RevenueCatProvider extends ChangeNotifier{
-  RevenueCatProvider(){
+enum Entitlement { free, exclusives }
+
+class RevenueCatProvider extends ChangeNotifier {
+  RevenueCatProvider() {
     init();
   }
 
   Entitlement _entitlement = Entitlement.free;
   Entitlement get entitlement => _entitlement;
-  Future init() async{
-    getPurchaseStatus();
-    Purchases.addPurchaserInfoUpdateListener((purchaserInfo) async{
+  Future init() async {
+    //getPurchaseStatus();
+    Purchases.addPurchaserInfoUpdateListener((purchaserInfo) async {
       updatePurchaseStatus();
     });
   }
 
-  Future getPurchaseStatus() async{
+  Future getPurchaseStatus() async {
     //print('eidarous1');
     PurchaserInfo restoredInfo = await Purchases.restoreTransactions();
     final entitlements = restoredInfo.entitlements.active.values.toList();
     print('restored entitlements $entitlements');
-    _entitlement = entitlements.isEmpty ? Entitlement.free : Entitlement.exclusives;
+    _entitlement =
+        entitlements.isEmpty ? Entitlement.free : Entitlement.exclusives;
     notifyListeners();
   }
-  Future updatePurchaseStatus() async{
+
+  Future updatePurchaseStatus() async {
     final purchaserInfo = await Purchases.getPurchaserInfo();
     final entitlements = purchaserInfo.entitlements.active.values.toList();
     print('entitlements $entitlements');
-    _entitlement = entitlements.isEmpty ? Entitlement.free : Entitlement.exclusives;
+    _entitlement =
+        entitlements.isEmpty ? Entitlement.free : Entitlement.exclusives;
     notifyListeners();
   }
 }
--- lib/services/purchase_api.dart
+++ lib/services/purchase_api.dart
@@ -1,50 +1,50 @@
+import 'package:Alhany/constants/constants.dart';
 import 'package:purchases_flutter/object_wrappers.dart';
 import 'package:purchases_flutter/purchases_flutter.dart';
 
-class PurchaseTracks{
+class PurchaseTracks {
   static const oneTrackPurchaseID = 'one_track';
   static const allTracksPurchaseID = 'all_tracks';
 
   static const allIds = [oneTrackPurchaseID, allTracksPurchaseID];
-
 }
-class PurchaseApi{
 
+class PurchaseApi {
   static const _apiKey = 'vMbzspZIBRgUofsCKgSlOawHxTstYeQl';
 
-  static Future init() async{
+  static Future init() async {
     await Purchases.setDebugLogsEnabled(true);
-    await Purchases.setup(_apiKey);
+    await Purchases.setup(_apiKey, appUserId: Constants.currentUserID);
   }
 
-  static Future<List<Offering>> fetchOffersByIds(List<String> ids) async{
+  static Future<List<Offering>> fetchOffersByIds(List<String> ids) async {
     final offers = await fetchOffers();
 
     return offers.where((offer) => ids.contains(offer.identifier)).toList();
   }
 
-  static Future<List<Offering>> fetchOffers({bool all = true}) async{
+  static Future<List<Offering>> fetchOffers({bool all = true}) async {
     try {
       final offerings = await Purchases.getOfferings();
-      if(!all) {
+      if (!all) {
         //print('offerings $offerings');
         final current = offerings.current;
         //print('offerings.current: $current');
 
         return [current] ?? [];
-      } else{
+      } else {
         return offerings.all.values.toList();
       }
-    }catch(e){
+    } catch (e) {
       return [];
     }
   }
 
-  static Future<bool> purchasePackage(Package package) async{
-    try{
+  static Future<bool> purchasePackage(Package package) async {
+    try {
       await Purchases.purchasePackage(package);
-    return true;
-    }catch(e){
+      return true;
+    } catch (e) {
       print('purchasePackage error: $e');
       return false;
     }
